03e1093148b29f5731a1afd824c41c572150c3d8
Vasanth macbook air <s.vasanthrojin@gmail.com>
2024-01-08 04:56:10 +0800

    rename for easier understanding

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import os
 from fastapi import FastAPI, Request
 import requests
 from dotenv import load_dotenv
+import logging
+logging.basicConfig(level=logging.INFO)
+
 load_dotenv()  # This loads the environment variables from .env
 
 
 API_KEY = os.environ.get("TELEGRAM_API_KEY")
-print("app started")
+logging.info("app started")
 app = FastAPI()
 
 @app.post("/webhook")
 async def webhook(req: Request):
-    print("something is detected")
+    logging.info("something is detected")
     body = await req.json()
     if 'message' not in body:
-        return print('No message found', body)
+        return logging.info('No message found', body)
     bm = body['message']
     if 'text' not in bm:
-        return print('No text found', body)
-    print("valid text wow")
+        return logging.info('No text found', body)
+    logging.info("valid text wow")
     chat_id = bm['chat']['id']
     text = bm['text']
     # Send echo message
@@ -28,5 +31,5 @@ async def webhook(req: Request):
         "text": text
     }
     response = requests.post(url, json=payload)
-    print("sent back")
+    logging.info("sent back")
     return response.json()
